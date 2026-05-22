e9d069461a55fcef87e9a369cf246c456d889671
Chris Strawser <46349138+mcochris@users.noreply.github.com>
2026-05-22 17:17:10 -0400

    UI changes, a WIP

--- src/main.ts
+++ src/main.ts
@@ -12,7 +12,7 @@ import "tabulator-tables/dist/css/tabulator.min.css";
 import "tabulator-tables/dist/css/tabulator_bootstrap5.min.css";
 // @ts-expect-error - bootstrap doesn't have type definitions
 import * as bootstrap from "bootstrap";
-// @ts-expect-error - bootstrap doesn't have type definitions
+// @ts-expect-error - tabulator doesn't have type definitions
 import { TabulatorFull as Tabulator } from 'tabulator-tables';
 import "./theme_toggle.js";
 
@@ -21,7 +21,7 @@ const currentPathDiv = document.querySelector("#currentPath") as HTMLDivElement;
 const musicFilesDiv = document.querySelector("#musicfiles") as HTMLDivElement;
 const backIconSvg = '<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-left" viewBox="0 0 16 16"><path fill-rule="evenodd" d="M15 8a.5.5 0 0 0-.5-.5H2.707l3.147-3.146a.5.5 0 1 0-.708-.708l-4 4a.5.5 0 0 0 0 .708l4 4a.5.5 0 0 0 .708-.708L2.707 8.5H14.5A.5.5 0 0 0 15 8"/></svg> ';
 const homeIconSvg = '<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-house" viewBox="0 0 16 16"><path d="M8.707 1.5a1 1 0 0 0-1.414 0L.646 8.146a.5.5 0 0 0 .708.708L2 8.207V13.5A1.5 1.5 0 0 0 3.5 15h9a1.5 1.5 0 0 0 1.5-1.5V8.207l.646.647a.5.5 0 0 0 .708-.708L13 5.793V2.5a.5.5 0 0 0-.5-.5h-1a.5.5 0 0 0-.5.5v1.293zM13 7.207V13.5a.5.5 0 0 1-.5.5h-9a.5.5 0 0 1-.5-.5V7.207l5-5z"/></svg> ';
-const favoriteIconSvg = '<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="red" class="bi bi-heart-fill" viewBox="0 0 16 16"><path fill-rule="evenodd" d="M8 1.314C12.438-3.248 23.534 4.735 8 15-7.534 4.736 3.562-3.248 8 1.314"/></svg> ';
+const favoriteIconSvg = '<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-heart" viewBox="0 0 16 16"><path d="m8 2.748-.717-.737C5.6.281 2.514.878 1.4 3.053c-.523 1.023-.641 2.5.314 4.385.92 1.815 2.834 3.989 6.286 6.357 3.452-2.368 5.365-4.542 6.286-6.357.955-1.886.838-3.362.314-4.385C13.486.878 10.4.28 8.717 2.01zM8 15C-7.333 4.868 3.279-3.04 7.824 1.143q.09.083.176.171a3 3 0 0 1 .176-.17C12.72-3.042 23.333 4.867 8 15"/></svg> ';
 let homeDirectory = "";
 let pathSeparator = "/";
 
@@ -297,10 +297,11 @@ async function listMusicFiles() {
 
 	new Tabulator("#musicfiles", {
 		data: tableData,
+		layout: "fitColumns",
 		columns: [
 			{ title: "ID", field: "id", visible: false },
 			{ title: "File (click to play/stop)", field: "filepath", formatter: pathNameFormatter },
-			{ title: "Rating", field: "rating", formatter: ratingFormatter, headerSort: false, minWidth: 150 }
+			{ title: "Rating", field: "rating", formatter: ratingFormatter, minWidth: 150 }
 		]
 	});
 
@@ -369,7 +370,7 @@ async function listMusicFiles() {
 
 				clearButton.type = "button";
 				clearButton.className = "btn btn-link p-0 border-0 ms-2";
-				clearButton.style.color = "#dc3545";
+				clearButton.style.color = "currentColor";
 				clearButton.dataset.action = "clear-rating";
 				clearButton.dataset.filePath = filePath;
 				clearButton.title = "Clear rating";
